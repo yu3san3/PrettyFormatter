ccd16d884d3189710ea11a397967ec83817a108a
yu3san3 <orange12817@yahoo.co.jp>
2025-01-24 23:29:36 +0900

    [update] Added Unit Test

--- Tests/PrettyFormatterTests/PrettyFormatterTests.swift
+++ Tests/PrettyFormatterTests/PrettyFormatterTests.swift
@@ -1,6 +1,39 @@
 import Testing
 @testable import PrettyFormatter
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+struct PrettyFormatterTests {
+    private struct User: Encodable {
+        let userName: String
+        let age: Int
+        let email: String
+    }
+
+    private let testDictionary: [String: Any] = [
+        "userName": "John Smith",
+        "age": 30,
+        "email": "john@smith.com"
+    ]
+    private let testUser = User(
+        userName: "John Smith",
+        age: 30,
+        email: "john@smith.com"
+    )
+
+    @Test("Test PrettyFormatter `prettyFormatted` Method")
+    func prettyFormatterPrettyFormattedMethodSuccess() async throws {
+        let formatted = PrettyFormatter().prettyFormatted(testDictionary)
+
+        #expect(formatted.contains("  \"userName\" : \"John Smith\""))
+        #expect(formatted.contains("  \"age\" : 30"))
+        #expect(formatted.contains("  \"email\" : \"john@smith.com\""))
+    }
+
+    @Test("Test Encodable `prettyFormatted` Method")
+    func encodablePrettyFormatterMethodSuccess() async throws {
+        let formatted = testUser.prettyFormatted
+
+        #expect(formatted.contains("  \"userName\" : \"John Smith\""))
+        #expect(formatted.contains("  \"age\" : 30"))
+        #expect(formatted.contains("  \"email\" : \"john@smith.com\""))
+    }
 }
