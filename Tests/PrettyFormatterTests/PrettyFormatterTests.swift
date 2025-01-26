e00ea5159187054bc8bca8b91b9b5f04b4d3effe
yu3san3 <orange12817@yahoo.co.jp>
2025-01-27 00:43:04 +0900

    [update] fixed test

--- Tests/PrettyFormatterTests/PrettyFormatterTests.swift
+++ Tests/PrettyFormatterTests/PrettyFormatterTests.swift
@@ -1,4 +1,5 @@
 import Testing
+import Foundation
 @testable import PrettyFormatter
 
 struct PrettyFormatterTests {
@@ -6,6 +7,7 @@ struct PrettyFormatterTests {
         let userName: String
         let age: Int
         let email: String
+        let creationDate: Date
     }
 
     private let testDictionary: [String: Any] = [
@@ -16,11 +18,12 @@ struct PrettyFormatterTests {
     private let testUser = User(
         userName: "John Smith",
         age: 30,
-        email: "john@smith.com"
+        email: "john@smith.com",
+        creationDate: Date(timeIntervalSince1970: 0)
     )
 
-    @Test("Test PrettyFormatter `prettyFormatted` Method")
-    func prettyFormatterPrettyFormattedMethodSuccess() async throws {
+    @Test("PrettyFormatter `prettyFormatted` Method Success")
+    func prettyFormatterPrettyFormattedMethodSuccess() {
         let formatted = PrettyFormatter().prettyFormatted(testDictionary)
 
         #expect(formatted.contains("  \"userName\" : \"John Smith\""))
@@ -28,12 +31,22 @@ struct PrettyFormatterTests {
         #expect(formatted.contains("  \"email\" : \"john@smith.com\""))
     }
 
-    @Test("Test Encodable `prettyFormatted` Method")
-    func encodablePrettyFormatterMethodSuccess() async throws {
+    @Test("PrettyFormatter `prettyFormatted` Method Failure")
+    func prettyFormatterPrettyFormattedMethodFailure() {
+        let date = Date() // `Date` itself is not converted as a valid JSON.
+        let formatted = PrettyFormatter().prettyFormatted(date)
+
+        #expect(JSONSerialization.isValidJSONObject(date) == false)
+        #expect(formatted == PrettyFormatterError.couldNotFormatToJSON.errorDescription)
+    }
+
+    @Test("Encodable `prettyFormatted` Property Success")
+    func encodablePrettyFormatterPropertyReturnsValidResult() {
         let formatted = testUser.prettyFormatted
 
         #expect(formatted.contains("  \"userName\" : \"John Smith\""))
         #expect(formatted.contains("  \"age\" : 30"))
         #expect(formatted.contains("  \"email\" : \"john@smith.com\""))
+        #expect(formatted.contains("  \"creationDate\" : \"1970-01-01 00:00:00 +0000\""))
     }
 }
